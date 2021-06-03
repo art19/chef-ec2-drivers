9eaf016faea2cc6972772d8dd98a995da8bf3954
Keith Gable <keith@art19.com>
2021-06-03 12:57:25 -0700

    We also need kernel-headers

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,10 +4,10 @@
 # Cookbook Name:: ec2-drivers
 # Recipe:: default
 #
-# Copyright 2020 ART19
+# Copyright 2021 ART19, Inc.
 #
 
-package %w[createrepo dkms kernel-devel rpm-build]
+package %w[createrepo dkms kernel-devel kernel-headers rpm-build]
 
 include_recipe '::localrepo'
 include_recipe '::ena'

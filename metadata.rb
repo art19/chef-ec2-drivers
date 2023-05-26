5d4d42f24d845b2f949ba5b6d867d8eabc3c674a
Johannes Vetter <johvette@amazon.com>
2023-05-26 11:58:20 -0700

    Bumped the cookbook version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'keith@art19.com'
 license          'MIT'
 description      'Cookbook that installs the latest versions of Amazon EC2 drivers'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.6'
+version          '0.0.7'
 
 chef_version '>= 15' if respond_to?(:chef_version)
 issues_url 'https://github.com/art19/chef-ec2-drivers/issues' if respond_to?(:issues_url)

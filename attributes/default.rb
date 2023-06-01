cab71bec374391be8b630915659f2710fda79396
Keith Gable <gablk@amazon.com>
2023-06-01 13:36:47 -0700

    Update ixgbevf and ena

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 default['ec2-drivers']['ena'] = {
-  'version'   => '2.8.6',
+  'version'   => '2.8.7',
   'install'   => true,
   'checksums' => {
     '2.2.11' => 'e01cf2456d399804593339507ebe89a197d6ed640e0730e0af25efb2681f6c60',
@@ -13,28 +13,31 @@ default['ec2-drivers']['ena'] = {
     '2.7.0'  => '74f5d86c55c7c50982c405661be613e3d2c3f28fba977743fd0b4d96fcf11d60',
     '2.7.1'  => '860119a062ef4662b05e2b270d231f588bf02b00cdeb3cef5e7cb7972449630f',
     '2.8.1'  => '527006843605f16be6344773313621b194dfbe3baead0b244f1c19069b617e87',
-    '2.8.6'  => '81bdaef76066a45c5fb41e09f398980e3540022ac95932f297900f7c6385880d'
+    '2.8.6'  => '81bdaef76066a45c5fb41e09f398980e3540022ac95932f297900f7c6385880d',
+    '2.8.7'  => '38f9cf18860c756112388a4469093218276f68d35ac173f584cc97a3c2584899'
   }
 }
 
 default['ec2-drivers']['ixgbevf'] = {
-  'version'            => '4.13.3',
+  'version'            => '4.17.11',
   'install'            => true,
   'checksums'          => {
-    '4.9.3'  => '0a74665ac3e5e41b9bb12ef06617f3019980d2a66b3d1c52c33d3845dd557013',
-    '4.10.2' => '699e9a78e474481fcbbfbad0a12d0edd704a19c6de173d3666c919c7e361868f',
-    '4.11.1' => '0031d09a54af9ecf1216c185e4641e38527643afa09e78d1bd95752e49fe8985',
-    '4.12.4' => 'd81f981dea85165057e92bd0d32c41f990890c57c35e2f0857229c9e490f7a1b',
-    '4.13.3' => 'b03010df2c491192dfda489fb6b5a99e83670ac9bc12fc76b1a144ddb2af3f76'
+    '4.9.3'   => '0a74665ac3e5e41b9bb12ef06617f3019980d2a66b3d1c52c33d3845dd557013',
+    '4.10.2'  => '699e9a78e474481fcbbfbad0a12d0edd704a19c6de173d3666c919c7e361868f',
+    '4.11.1'  => '0031d09a54af9ecf1216c185e4641e38527643afa09e78d1bd95752e49fe8985',
+    '4.12.4'  => 'd81f981dea85165057e92bd0d32c41f990890c57c35e2f0857229c9e490f7a1b',
+    '4.13.3'  => 'b03010df2c491192dfda489fb6b5a99e83670ac9bc12fc76b1a144ddb2af3f76',
+    '4.17.11' => '59813a6f92e699b4cb6983b70b91137eece4609a113fa7bb60db0be9889d2b46'
   },
   # See: https://downloadcenter.intel.com/download/18700/Ethernet-Intel-Network-Adapter-Virtual-Function-Driver-for-Intel-10-Gigabit-Ethernet-Network-Connections
   # This is the ID in the downloadmirror.intel.com download URL + /eng if it's there
   'intel_download_ids' => {
-    '4.9.3'  => '30241/eng',
-    '4.10.2' => '30274/eng',
-    '4.11.1' => '18700/eng',
-    '4.12.4' => '18700/eng',
-    '4.13.3' => '682694'
+    '4.9.3'   => '30241/eng',
+    '4.10.2'  => '30274/eng',
+    '4.11.1'  => '18700/eng',
+    '4.12.4'  => '18700/eng',
+    '4.13.3'  => '682694',
+    '4.17.11' => '773700'
   }
 }
 
